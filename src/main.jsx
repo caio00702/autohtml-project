dd3419bdcf3bd135dc6ef4e011d7830548c1a4aa
CAIO EMANUEL SILVA <77758820+caio00702@users.noreply.github.com>
2025-01-15 11:48:12 -0300

    Ajusta configurações para GitHub Pages

--- src/main.jsx
+++ src/main.jsx
@@ -3,13 +3,16 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import { HashRouter } from 'react-router-dom'
 import { MoviesProvider } from './contexts/MoviesContext'
+import { ThemeProvider } from 'styled-components'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <HashRouter>
-      <MoviesProvider>
-        <App />
-      </MoviesProvider>
+    <HashRouter basename="/">
+      <ThemeProvider theme={{}}>
+        <MoviesProvider>
+          <App />
+        </MoviesProvider>
+      </ThemeProvider>
     </HashRouter>
   </React.StrictMode>,
 ) 
--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,9 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "./",
+  base: "/autohtml-project/",
+  build: {
+    outDir: "dist",
+    assetsDir: "assets",
+  },
 });
